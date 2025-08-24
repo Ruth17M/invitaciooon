f5ff951246280f9f56d093dbaf398a2bdf32b02f
Ruth17M <manriquezruth2004@gmail.com>
2025-08-23 19:29:28 -0600

    invitados actualizados

--- bd.py
+++ bd.py
@@ -100,6 +100,15 @@ def eliminar_invitado(id):
     conn.close()
     print(f"🗑️ Invitado con ID {id} eliminado.")
 
+ # Eliminar todos los invitados
+def eliminar_todos_invitados():
+        conn = sqlite3.connect("database.sqlite")
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM invitados;")
+        conn.commit()
+        conn.close()
+        print("🗑️ Todos los invitados han sido eliminados.")
+
 
 if __name__ == "__main__":
    # crear_base_datos()
@@ -111,105 +120,103 @@ if __name__ == "__main__":
     # 2. Agregar muchos a la vez
     nuevos_invitados = [
         ("Ruth Manríquez", 2, "Ruth Manríquez, Ethan Lemus"),
-        ("Juan Lemus", 5, " Juan Lemus, Adriana Montelongo, Oziel Lemuz, Maggie Chavez"),
+        ("Juan Lemus", 4, " Juan Lemus, Adriana Montelongo, Oziel Lemus, Maggie Chavez"),
         ("Jaime Fuerte", 4, "Jaime Fuerte, Rosario Gómez, Jonas Fuerte, Job Fuerte"),
-        ("Jesse Fuerte", 3, "Jesse Fuerte, Elvia Salas, Amy Fuerte"),
-        ("Rosario Fuerte", 4, "Rosario Fuerte, Yaretzi Fuerte, Citlalli Fuerte, Saul Fuerte"),
-        ("Herminia Tavera", 7, "Herminia Tavera, Juan Fuerte, Magda Fuerte, Caro Fuerte, Diego Fuerte, + 2"),
+        ("Jesse Cruz", 3, "Jesse Cruz, Elvia Salas, Amelia Fuerte"),
+        ("Rosario Fuerte", 4, "Rosario Fuerte, Yaretzi Cruz, Citlalli Cruz, Saul Cruz"),
+        ("Herminia Tavera", 7, "Herminia Tavera, Juan Fuerte, Magda Fuerte, Caro Fuerte, Diego Fuerte, Jesus Rios, Denisse"),
         ("Cesar Aranda", 2, "Cesar Aranda, Denisse Ontiveros"),
-        ("Melissa Aranda", 2, "Melissa Aranda, Humberto Becerra, Matias Becerra"),
-        ("Gammaliel Fuerte", 8, "Gammaliel Fuerte, Coco Muñoz, Jafet Fuerte, Josue Fuerte, Judith Fuerte, Hassiel Fuerte, + 2"),
-        ("Juanita Tavera", 4, "Juanita Tavera, Fer Aranda, Rene Cardona"),
-        ("Violeta Tavera", 5, "Violeta Tavera, + 4"),
-        ("Rosario Fuerte", 4, "Rosario Fuerte, Yaretzi Fuerte, Citlalli Fuerte, Saul Fuerte"),
-        ("Jose Luis Chavez", 5, "Jose Luis Chavez, +4"),
-        ("Familia Irena Rocha", 5, "Laura Rocha, +1"),
-        ("Familia Salgado", 2, "+2"),
+        ("Melissa Aranda", 3, "Melissa Aranda, Humberto Becerra, Matias Becerra"),
+        ("Gammaliel Fuerte", 8, "Gammaliel Fuerte, Coco Muñoz, Jafet Fuerte, Josue Fuerte, Judith Fuerte, Hassiel Fuerte, Daniela Villagomez, Sandra Colorado"),
+        ("Juanita Tavera", 4, "Juanita Tavera, Fer Aranda, Rene Cardona, Manuel Aranda"),
+        ("Violeta Tavera", 5, "Violeta Tavera, Jesus Campos, Jonathan Campos, Veronica Campos, Angel Campos"),
+        ("Jose Luis Chavez", 2, "Jose Luis Chavez, Lupita"),
+        ("Pedro Salgado", 2, "Pedro Salgado, Rocio"),
         ("Martha Rocha", 1, "Martha Rocha"),
         ("Jaziel Miranda", 2, "Jaziel Miranda, Greys Padilla"),
         ("Laura Rocha", 2, "Laura Rocha, Martin Irena"),
-        ("Familia Manriquez Fuerte", 9, "Mariano Manriquez, Miriam Fuerte, Ruth Manriquez, Baruc Manriquez, Mateo Manriquez, Fatima Manriquez, Esaú Manriquez, Guadalupe Reyes, Lucita Manriquez"),
-        ("Ulises Escalante", 2, "Ulises Escalante, +1 adulto"),
-        ("Ulises Vargas",3, "Ulises Vargas, +1 adulto y 1 niño"),
+        ("Familia Manriquez Fuerte", 8, "Mariano Manriquez, Miriam Fuerte, Baruc Manriquez, Mateo Manriquez, Fatima Manriquez, Esaú Manriquez, Guadalupe Reyes, Lucita Manriquez"),
+        ("Ulises Escalante", 2, "Ulises Escalante, esposa"),
         ("Alex Balderas", 1, "Alex Balderas"),
-        ("Marlene", 2, "Marlene, +1"),
-        ("César", 1, "César"),
-        ("Belen Porras", 3, "Belen Porras, Juan Diaz, Nelly Diaz"),
-        ("Juan de Dios Campos", 2, "Juan de Dios Campos, Gabriela, +1 niño"),
-        ("José de Jesús Campos", 2, "José de Jesús Campos, Magy, +2 niños"),
-        ("Emmanuel Campos", 2, "Emmanuel Campos, +1 adulto y 1 niño"),
-        ("Pbr. Juan Ramírez", 1, "Pbr. Juan Ramírez"),
-        ("Sr. Cura David Alba", 1, "Sr. Cura David Alba"),
+        ("Marlenne Covarrubias", 1, "Marlenne Covarrubias"),
+        ("César Limon", 1, "César Limon"),
+        ("Berenice Porras", 3, "Berenice Porras, Juan Diaz, Nelly Diaz"),
+        ("Juan de Dios Campos", 3, "Juan de Dios Campos, Gabriela, +1 niño"), 
+        ("José de Jesús Campos", 4, "José de Jesús Campos, Magy, +2 niños"),
+        ("Emmanuel Campos", 4, "Emmanuel Campos, +2 adultos y 1 niño"), 
+        ("Juan Ramírez", 1, "Juan Ramírez"),
+        ("David Alba", 1, "David Alba"),
         ("Martha Diaz", 2, "Martha Diaz, Ricardo Espinoza"),
         ("Alexa Azpeitia", 1, "Alexa Azpeitia"),
-        ("Ana Reyes", 1, "Ana Reyes"),        
+        ("Ana Reyes Zermeño", 1, "Ana Reyes Zermeño"),        
         ("Andrea Godinez", 1, "Andrea Godinez"),
         ("Maura de la Torre", 1, "Maura de la Torre"),
         ("Leonardo Martinez", 1, "Leonardo Martinez"),
         ("Milton Santoyo", 1, "Milton Santoyo"),
         ("Nubia Velazquez", 1, "Nubia Velazquez"),
         ("Regina Martinez", 1, "Regina Martinez"),
-        ("Erick", 1, "Erick"),
+        ("Erick Torres", 1, "Erick Torres"), 
         ("Sandra Yunuen", 1, "Sandra Yunuen"),
         ("Saúl Lara", 1, "Saúl Lara"),
         ("Yaretzi Paola", 1, "Yaretzi Paola"),
         ("Regina Lozano", 1, "Regina Lozano"),
         ("Hasiel Lozano", 1, "Hasiel Lozano"),
         ("Mariana Jauregui", 1, "Mariana Jauregui"),
-        ("Liz", 1, "Liz"),
+        ("Lizbeth Lopez", 1, "Lizbeth Lopez"),
         ("Regina", 1, "Regina"),
         ("Santiago Flores", 1, "Santiago Flores"),
-        ("Evan", 1, "Evan"),
+        ("Evan", 1, "Evan"), 
         ("Santiago Flores", 1, "Santiago Flores"),
-        ("Evan", 1, "Evan"),
         ("Mauro Moreno", 1, "Mauro Moreno"),
-        ("Ana", 1, "Ana"),
+        ("Ana", 1, "Ana"), 
         ("Jorge Herrera", 1, "Jorge Herrera"),
         ("Yojan Ramos", 1, "Yojan Ramos"),
         ("Omar Guerra", 1, "Omar Guerra"),
-        ("Flor Hidalgo", 1, "Flor Hidalgo"),
-        ("Diana GJ", 1, "Diana GJ"),
-        ("Dulce", 3, "Dulce, Issac, Emmanuel GJ"),
-        ("Emilio GJ", 1, "Emilio GJ"),
-        ("Karol GJ", 1, "Karol GJ"),
-        ("Oscar GJ", 2, "Oscar GJ, Tere GJ"),
-        ("Lucia GJ", 1, "Lucia GJ"),
-        ("Sayan GJ", 1, "Sayan GJ"),
-        ("Familia Aranda Guevara", 5, "Familia Aranda Guevara (5 adultos)"),
-        ("Aranza", 1, "Aranza"),
+        ("Diana Zamarripa", 1, "Diana Zamarripa"),
+        ("Dulce Medina", 3, "Dulce Medina, Issac Medina, Emmanuel Medina"), 
+        ("Oscar Balandran", 2, "Oscar Balandran, Tere Rodriguez"), 
+        ("Lucia Covarrubias", 1, "Lucia Covarrubias"), 
+        ("Rodrigo Aranda", 5, "Rodrigo Aranda, Fernanda Aranda, Mariana Aranda, Rosy Guevara, Rodrigo Aranda"),
+        ("Aranza Cruz", 1, "Aranza Cruz"),
         ("Santiago Fuentes", 1, "Santiago Fuentes"),
-        ("Daniela", 1, "Daniela"),
-        ("Alondra", 1, "Alondra"),
+        ("Daniela Hernandez", 1, "Daniela Hernandez"), 
+        ("Alondra", 1, "Alondra"), 
         ("Alejandro Mendoza", 1, "Alejandro Mendoza"),
         ("Fernando Ayala", 1, "Fernando Ayala"),
         ("Mireya Vazquez", 2, "Mireya Vazquez, Andrea Vazquez"),
         ("Alma Delia Tovar", 2, "Alma Delia Tovar, Alma Tovar"),
         ("Juan Reyes", 2, "Juan Reyes, Adelita"),
         ("Pablo Reyes", 2, "Pablo Reyes, Lupita"),
-        ("Omar Reyes", 2, "Omar Reyes, +1"),
+        ("Omar Reyes", 2, "Omar Reyes, esposa"),
         ("Mary Reyes", 2, "Mary Reyes, Manuel"),
-        ("Coco Reyes", 2, "Coco Reyes, +1"),
-        ("Fany Reyes", 2, "Fany Reyes, Ani Reyes"),
-        ("Don Chano", 1, "Don Chano"),
+        ("Coco Reyes", 2, "Coco Reyes, +1 adulto"),
+        ("Fany Reyes", 3, "Fany Reyes, Ani Reyes, Maximiliano Reyes"),
         ("Cata Reyes", 2, "Cata Reyes, Yolanda Reyes"),
-        ("Angelica Fuerte", 4, "Angelica Fuerte,Diana Fuerte, Jessenia Fuerte, José, +1 niño"),
-        ("Carlos", 2, "Carlos, Brenda"),
-        ("Lola", 4, "Lola Reyes, +3 adultos"),
-        ("Ernesto", 5, "Ernesto, Lucy, Pablo, Angela, Marcela"),
-        ("Marlene Covarrubias", 1, "Marlene Covarrubias"),
-        ("Leticia Patiño", 1, "Leticia Patiño"),
+        ("Angelica Fuerte", 5, "Angelica Fuerte,Diana Fuerte, Jessenia Fuerte, José, +1 niño"),
+        ("Carlos", 2, "Carlos Morales, Brenda Morales"),
+        ("Lola Reyes", 4, "Lola Reyes, +3 adultos"),
+        ("Ernesto Velazquez", 5, "Ernesto Velazquez, Lucy Muñoz, Jose Pablo Velazquez, Angela Velazquez, Marcela Velazquez"),
+        ("Leticia Patiño", 1, "Leticia Patiño"), 
         ("Ulises Vargas", 3, "Ulises Vargas, Laura, 1 niño"),
-        ("Trini", 1, "Trini"),
-        ("Elizabeth", 2, "Elizabeth, 1 adulto"),
-        ("Fabian", 1, "Fabian"),
-        ("Esther",2, "Esther, +1 adulto"),
-        ("Claudia", 1, "Claudia"),
-        ("Maestra Paty", 1, "Maestra Paty"),
-        ("Mario Jasso", 2, "Mario Jasso, +1"),
-        ("Ximena Martinez",1, "Ximena Martinez")
+        ("Trinidad  Martinez", 1, "Trinidad  Martinez"), 
+        ("Elizabeth", 2, "Elizabeth, +1 adulto"),
+        ("Fabian", 1, "Fabian"), 
+        ("Esther",2, "Esther, +1 adulto"), 
+        ("Claudia", 1, "Claudia"), 
+        ("Martha", 1, "Martha"), 
+        ("Mario Jasso", 1, "Mario Jasso"),
+        ("Ximena Martinez",1, "Ximena Martinez"),
+        ("Omar Morales", 1, "Omar Morales"),
+        ("Maria Castillo", 1, "Maria Castillo"),
+        ("Josafat Aguirre", 1, "Josafat Aguirre"),
+        ("Andres Aguilera", 1, "Andres Aguilera"),
+        ("Camila Liedo", 1, "Camila Liedo"),
+        ("Mariana Ortiz", 1, "Mariana Ortiz")
 
 
     ]
+    
+
     
     agregar_varios_invitados(nuevos_invitados)
 
@@ -217,8 +224,10 @@ if __name__ == "__main__":
 
 
     # editar_invitado(2, nuevo_nombre="Cristina Ramírez", nuevos_pases=4)
+   
 
-    #eliminar_invitado(9)
+    # Llama a la función para eliminar todos los invitados
+    #eliminar_todos_invitados()
     
 
     ver_invitados()
